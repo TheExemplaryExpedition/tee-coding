ccf9d96ea3e019efe534a8b4059cb188d037d5d5
Shreedhar Kodate <shreedhar.kodate.tee@gmail.com>
2023-09-12 22:03:39 -0700

    Exploring various options to draw simple text and rectangle - Idea to create a resume template

--- src/working_with_pdfs/expt_001_create_pdf.py
+++ src/working_with_pdfs/expt_001_create_pdf.py
@@ -26,12 +26,53 @@ DIR_OUTPUT = "../../outputs"
 
 
 def draw(c):
+    str_out = "".join(["a"] * 100)
+    # c.setTitle("Trial")
+
+    c.drawRightString(100, 400, str_out)
+    c.drawAlignedString(100, 600, str_out)
+    c.drawCentredString(100, 500, str_out)
     c.drawString(100, 100, "Hello World")
+    c.drawString(400, 100, str_out)
+
+    # textobject = canvas.beginText(x, y)
+    # canvas.drawText(textobject)
+
+
+PS_FONT_NAME = "Times-Roman"
+FONT_SIZE = 12
+LINE_WIDTH = 10
+LINE_CAP = ""
+LINE_JOIN = ""
+MITER_LIMIT = 0
+PAGE_SIZE = (595.26, 841.88)  # page size for A4 -- points 1/72 inch
+
+
+def page_setup(c):
+    # To check the available font styles list
+    # avail_fonts = c.getAvailableFonts()
+
+    # Guiding box
+    x = 25  # round(PAGE_SIZE[0] // 2, 2)
+    y = 25  # round(PAGE_SIZE[1] // 2, 2)
+    width = PAGE_SIZE[0] - 50
+    height = PAGE_SIZE[1] - 50
+    c.rect(x, y, width, height, stroke=1, fill=0)
+
+    c.setFont(PS_FONT_NAME, FONT_SIZE, leading=None)
+    c.setLineWidth(LINE_WIDTH)
+    c.setPageSize(PAGE_SIZE)
+    # canvas.setLineCap(LINE_CAP)
+    # canvas.setLineJoin(LINE_JOIN)
+    # canvas.setMiterLimit(MITER_LIMIT)
+    # canvas.setDash(self, array=[], phase=0)
 
 
 def create_pdf(filename="hello.pdf"):
     filepath = os.path.join(DIR_OUTPUT, filename)
     c = canvas.Canvas(filepath)
+
+    page_setup(c)
     draw(c)
     c.showPage()
     c.save()
