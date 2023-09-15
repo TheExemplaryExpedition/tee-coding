c772d45408911203283ffe851efcdda58f6fe28a
Shreedhar Kodate <shreedhar.kodate.tee@gmail.com>
2023-09-14 21:06:18 -0700

    Update resume_format.py - Add LineCounter Singleton class to track current line position to add content

--- src/working_with_pdfs/resume_format.py
+++ src/working_with_pdfs/resume_format.py
@@ -9,6 +9,38 @@ LINE_CAP = ""
 LINE_JOIN = ""
 MITER_LIMIT = 0
 PAGE_SIZE = (595.26, 841.88)  # page size for A4 -- points 1/72 inch
+LINE_HEIGHT = 15
+MARGIN = 25
+
+
+# class SingletonClass(object):
+#   def __new__(cls):
+#     if not hasattr(cls, 'instance'):
+#       cls.instance = super(SingletonClass, cls).__new__(cls)
+#     return cls.instance
+
+
+class LineCounter:
+    """
+    Create a singleton class to keep track of the current line in a page
+    Reference: https://www.geeksforgeeks.org/singleton-pattern-in-python-a-complete-guide/
+    """
+
+    def __new__(cls):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(LineCounter, cls).__new__(cls)
+            cls.line_counter = 0
+            cls.coord_y = PAGE_SIZE[1]
+        return cls.instance
+
+    def next_line(self):
+        if self.line_counter == 0:
+            self.line_counter += 1
+            self.coord_y -= self.line_counter * 3 * LINE_HEIGHT
+        else:
+            self.coord_y -= LINE_HEIGHT
+            self.line_counter += 1
+        return self.coord_y
 
 
 class ResumeFormat:
@@ -24,19 +56,43 @@ class ResumeFormat:
         self.filepath = os.path.join(DIR_OUTPUT, filename)
         # Create a Resume Format Canvas object named as rfc
         self.rfc = canvas.Canvas(self.filepath)
+        self.lc = LineCounter()
 
-        self.draw_guiding_box()
+        self.draw_guiding_lines()
         self.init_doc_properties()
 
-    def draw_guiding_box(self):
+    def draw_guiding_lines(self):
         # Guiding box
-        x = 25  # round(PAGE_SIZE[0] // 2, 2)
-        y = 25  # round(PAGE_SIZE[1] // 2, 2)
-        width = PAGE_SIZE[0] - 50
-        height = PAGE_SIZE[1] - 50
+        x = MARGIN  # round(PAGE_SIZE[0] // 2, 2)
+        y = MARGIN  # round(PAGE_SIZE[1] // 2, 2)
+        width = PAGE_SIZE[0] - 2 * MARGIN
+        height = PAGE_SIZE[1] - 2 * MARGIN
 
         # Draw a rectangle to get the reference of where the text fits
-        self.rfc.rect(x, y, width, height, stroke=1, fill=0)
+        # self.rfc.rect(x, y, width, height, stroke=1, fill=0)
+
+        # self.rfc.lines(linelist)
+        list_x = [50 * ith for ith in range(1, 12)]
+        list_y = [50 * ith for ith in range(1, 16)]
+        # self.rfc.grid(list_x, list_y)
+
+    def draw_horizontal_line(self, x_left=None, x_right=None, y=None):
+        """
+        Draw a horizontal line separator to distinguish sections.
+        :param y: fixed because we need horizontal line
+        :param x_left: left end of the line segment
+        :param x_right: right end of the line segment
+        :return:
+        """
+        self.rfc.setLineWidth(0.5)
+        if x_left is None:
+            x_left = MARGIN - 5
+        if x_right is None:
+            x_right = PAGE_SIZE[0] - MARGIN + 5
+        if y is None:
+            y = self.lc.next_line() + LINE_HEIGHT // 2
+        self.rfc.line(x_left, y, x_right, y)
+        return self
 
     def init_doc_properties(self):
         # To check the available font styles list
@@ -58,12 +114,12 @@ class ResumeFormat:
         dict_font_styles = {
             "title": {
                 "font_name": "Times-Roman",
-                "font_size": 16
+                "font_size": 20
             },
 
             "subtitle": {
                 "font_name": PS_FONT_NAME,
-                "font_size": 12
+                "font_size": 10
             },
 
             "heading": {
@@ -93,59 +149,56 @@ class ResumeFormat:
         self.set_font_style(text_type="title")
         str_title = f"{firstname} {lastname}"
 
-        coord_x = round(PAGE_SIZE[0] // 2, 2)
-        coord_y = PAGE_SIZE[1] - 25
+        coord_x = round(PAGE_SIZE[0] / 2, 2)
+        coord_y = self.lc.next_line() + LINE_HEIGHT * 3 // 4
 
         self.rfc.drawCentredString(coord_x, coord_y, str_title)
 
         return self
 
-    def add_subtitle(self, str_sub_title, line_num=2):
+    def add_subtitle(self, str_sub_title):
         """
         Add sub title for the resume
         :param str_sub_title:
-        :param line_num:
         :return:
         """
         self.set_font_style(text_type="subtitle")
 
-        coord_x = round(PAGE_SIZE[0] // 2, 2)
-        coord_y = PAGE_SIZE[1] - line_num * 25
+        coord_x = round(PAGE_SIZE[0] / 2, 2)
+        coord_y = self.lc.next_line()
 
         self.rfc.drawCentredString(coord_x, coord_y, str_sub_title)
 
         return self
 
-    def add_heading(self, str_heading, line_num=3):
+    def add_heading(self, str_heading):
         """
         Add heading for a section of the resume
         :param str_heading:
-        :param line_num:
         :return:
         """
         self.set_font_style(text_type="heading")
 
-        coord_x = 25
-        coord_y = PAGE_SIZE[1] - line_num * 25
+        coord_x = MARGIN
+        coord_y = self.lc.next_line()
 
         self.rfc.drawString(coord_x, coord_y, str_heading)
 
         return self
 
-    def add_description(self, str_left, str_right, line_num=4):
+    def add_description(self, str_left, str_right):
         """
         Common type of Descriptions have str_left and str_right.
         Sometimes str_right can be empty, so it's fine.
         :param str_left:
         :param str_right:
-        :param line_num:
         :return:
         """
         self.set_font_style(text_type="description")
 
-        coord_x_left = 25
-        coord_x_right = PAGE_SIZE[0] - 25
-        coord_y = PAGE_SIZE[1] - line_num * 25
+        coord_x_left = MARGIN
+        coord_x_right = PAGE_SIZE[0] - MARGIN
+        coord_y = self.lc.next_line()
 
         self.rfc.drawString(coord_x_left, coord_y, str_left)
         self.rfc.drawRightString(coord_x_right, coord_y, str_right)
@@ -171,16 +224,14 @@ class ResumeData:
 
     def get_heading(self):
         dict_values = {
-            "str_heading": "Experience",
-            "line_num": 3
+            "str_heading": "Experience"
         }
         return dict_values
 
     def get_description(self):
         dict_values = {
-            "str_left": "CompanyName",
-            "str_right": "Location",
-            "line_num": 4
+            "str_left": "COMPANY NAME | Machine Learning Engineer",
+            "str_right": "Location | MMM. YYYY -- MMM. YYYY"
         }
         return dict_values
 
@@ -190,12 +241,26 @@ def main():
     obj_rd = ResumeData()
 
     obj_rf.add_title("FirstName", "LastName")
-
     obj_rf.add_subtitle(obj_rd.get_subtitle())
+    obj_rf.draw_horizontal_line()
+
     obj_rf.add_heading(**obj_rd.get_heading())
     obj_rf.add_description(**obj_rd.get_description())
+    obj_rf.draw_horizontal_line()
 
     obj_rf.save_resume()
+
+
+def check_line_counter():
+    lc = LineCounter()
+    print(lc.next_line())
+    print(lc.next_line())
+    print(lc.next_line())
+
+    lc2 = LineCounter()
+    print(lc.next_line())
+    print(lc.next_line())
+    print(lc.next_line())
 
 
 if __name__ == '__main__':
