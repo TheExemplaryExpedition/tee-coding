f7a83a2ec279d22fcc51f0c26677f771bbb674ca
Shreedhar Kodate <shreedhar.kodate.tee@gmail.com>
2023-09-22 15:20:16 -0700

    Update yaml_resume_reader.py - Add yaml file reading and printing the data

--- src/working_with_pdfs/yaml_resume_reader.py
+++ src/working_with_pdfs/yaml_resume_reader.py
@@ -2,12 +2,21 @@ import yaml
 
 
 class ResumeReader:
-    def __init__(self):
-        pass
+    def __init__(self, filepath):
+        self.filepath = filepath
+        self.dict_data = self.read_data()
+
+    def read_data(self):
+        with open(self.filepath, "r") as fh:
+            dict_data = yaml.safe_load(fh)
+
+        return dict_data
 
 
 def main():
-    pass
+    filepath = "yaml_files/sample_resume.yaml"
+    obj_rr = ResumeReader(filepath)
+    print(obj_rr.dict_data)
 
 
 if __name__ == '__main__':
