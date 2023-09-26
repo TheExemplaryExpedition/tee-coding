fca41c3e59bd79142e58278fc5d5d7b0b8870ce3
Shreedhar Kodate <shreedhar.kodate.tee@gmail.com>
2023-09-25 18:34:41 -0700

    Update yaml_resume_reader.py to create basic getter functions - Expand the ResumeReader class

--- src/working_with_pdfs/yaml_resume_reader.py
+++ src/working_with_pdfs/yaml_resume_reader.py
@@ -12,11 +12,44 @@ class ResumeReader:
 
         return dict_data
 
+    def get_first_name(self):
+        return self.dict_data["first_name"]
+
+    def get_last_name(self):
+        return self.dict_data["last_name"]
+
+    def get_full_name(self):
+        fn = self.get_first_name()
+        ln = self.get_last_name()
+        return f"{fn} {ln}"
+
+    def get_phone_number(self):
+        return self.dict_data["phone_number"]
+
+    def get_email_address(self):
+        return self.dict_data["email_address"]
+
+    def get_github_username(self):
+        return self.dict_data["github_username"]
+
+    def get_linkedin_username(self):
+        return self.dict_data["linkedin_username"]
+
+    def get_experiences(self):
+        return self.dict_data["experiences"]
+
+    def get_education(self):
+        return self.dict_data["education"]
+
+    def get_skills(self):
+        return self.dict_data["skills"]
+
 
 def main():
-    filepath = "yaml_files/sample_resume.yaml"
+    filepath = "yaml_files/minimalist_resume.yaml"
     obj_rr = ResumeReader(filepath)
     print(obj_rr.dict_data)
+    print(obj_rr.get_full_name())
 
 
 if __name__ == '__main__':
